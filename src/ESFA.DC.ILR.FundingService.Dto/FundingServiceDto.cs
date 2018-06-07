065e6b2a7fe70d47a02ac53d814d5188573115b4
Sai Siddula <Sai.Siddula@gmail.com>
2018-06-07 10:35:37 +0100

    Added Dto solution to the repo

---
+++ src/ESFA.DC.ILR.FundingService.Dto/FundingServiceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ESFA.DC.ILR.FundingService.Dto.Interfaces;
+using ESFA.DC.ILR.Model.Interface;
+
+namespace ESFA.DC.ILR.FundingService.Dto
+{
+    public class FundingServiceDto : IFundingServiceDto
+    {
+        public virtual IMessage Message { get; set; }
+
+        public virtual string[] ValidLearners { get; set; }
+    }
+}
---
+++ src/ESFA.DC.ILR.FundingService.Dto/Interfaces/IFundingServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ESFA.DC.ILR.Model.Interface;
+
+namespace ESFA.DC.ILR.FundingService.Dto.Interfaces
+{
+    public interface IFundingServiceDto
+    {
+        IMessage Message { get; }
+
+        string[] ValidLearners { get; }
+    }
+}
