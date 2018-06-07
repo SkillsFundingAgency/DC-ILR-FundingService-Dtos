d44a88caa3308c648ffff2f7e0103a4a5ccb15cd
Sai Siddula <Sai.Siddula@gmail.com>
2018-06-07 12:42:24 +0100

    added referencedataconfig

---
+++ src/ESFA.DC.ILR.FundingService.Dto/ReferenceDataConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFA.DC.ILR.FundingService.Dto
+{
+    public class ReferenceDataConfig
+    {
+        public string LARSConnectionString { get; set; }
+        public string PostCodeConnectionString { get; set; }
+
+    }
+}
