4794ec2eaf551ef2574ea7fadb324fb6c5e05614
Sai Siddula <Sai.Siddula@gmail.com>
2018-06-07 12:49:30 +0100

    fixed stylecop issues

--- src/ESFA.DC.ILR.FundingService.Dto/ReferenceDataConfig.cs
+++ src/ESFA.DC.ILR.FundingService.Dto/ReferenceDataConfig.cs
@@ -7,7 +7,7 @@ namespace ESFA.DC.ILR.FundingService.Dto
     public class ReferenceDataConfig
     {
         public string LARSConnectionString { get; set; }
-        public string PostCodeConnectionString { get; set; }
 
+        public string PostCodeConnectionString { get; set; }
     }
 }
